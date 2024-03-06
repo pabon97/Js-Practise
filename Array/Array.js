73bb7ce5170769e09e5b121e53b7b07a0b8b647c
pabon98 <pabon@orangetoolz.com>
2024-03-06 09:38:19 +0600

    some array methods map/filter added

---
+++ Array/Array.js
@@ -0,0 +1,15 @@
+const cars = ["toyota", "volvo", "BMW"];
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+const fruits = [];
+fruits[0] = "Mango";
+fruits[1] = "Orange";
+fruits[2] = "Banana";
+console.log(fruits);
+
+//Arrays are special type of object
+const Person = ["John", "Doe", 46];
+console.log(typeof Person);
+
---
+++ Array/ArrayMap.js
@@ -0,0 +1,5 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.map((fruit)=>{
+    console.log(fruit)
+})
