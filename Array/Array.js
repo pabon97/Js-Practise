f08f6efe40b04e9632f20d08ad59d380a5251982
pabon98 <pabon@orangetoolz.com>
2024-03-08 21:10:40 +0600

    added array methods

--- Array/Array.js
+++ Array/Array.js
@@ -9,7 +9,8 @@ fruits[1] = "Orange";
 fruits[2] = "Banana";
 console.log(fruits);
 
-//Arrays are special type of object
-const Person = ["John", "Doe", 46];
+//Arrays are special type of object. if we console type of array we will see this.
+const Person = ["John", "Doe", 46, 50];
+console.log(Person);
 console.log(typeof Person);
 
