694acfffc330149571a74d46a716a8c43cce9452
pabon98 <pabon@orangetoolz.com>
2024-03-09 11:49:13 +0600

    Array reduce, some & every method added

--- Array/ArrayReduce.js
+++ Array/ArrayReduce.js
@@ -1,3 +1,4 @@
+//Example 1
 const array = [1, 2, 3, 4];
 
 const initialValue = 0;
@@ -8,4 +9,13 @@ const sumWithInitial = array.reduce(
 );
 
 console.log(sumWithInitial);
+
+// Example 2, Here initial value is 10 which is first index of numbers array
+
+const numbers = [10, 20, 30, 40, 50];
+
+let sum = numbers.reduce((previousValue, currentvalue, index, array)=> {
+  return previousValue + currentvalue
+})
+console.log(sum)
 
