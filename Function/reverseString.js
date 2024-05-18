4ae23a59a3c4b2426edcf251a35e80b09a747456
pabon98 <pabon@orangetoolz.com>
2024-05-18 20:25:09 +0600

    added palindrome & upperCase function

--- Function/reverseString.js
+++ Function/reverseString.js
@@ -1,6 +1,6 @@
 // write a function to reverse a string
 
-// // Solution 1
+//  Solution 1
 function ReverseString (string){
  let reversedString = ""
  for (let i = string.length - 1; i>=0 ; i--){
@@ -13,10 +13,10 @@ console.log(ReverseString('Extraction'));
 console.log(ReverseString('Notice Period'));
 
 // Solution 2
-
 const reverseString = (item)=>{
-   let reversed = [...item].reverse().join();
-   console.log(reversed);
+   let splitArray = item.split('')
+   let reversed = splitArray.reverse().join("");
+   return reversed
 }
 
 console.log(reverseString('javascript'));
